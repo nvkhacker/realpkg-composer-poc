c5aca3548f0285cdf377a0160bd5c7f97ecbe9dc
nvkhacker <naveenkumawat09@gmail.com>
2023-08-09 18:27:54 +0530

    composer files added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'realpkg/pkg-name',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9717475b23af9197fa78b80cd47159bf7293bd03',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'realpkg/pkg-name' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9717475b23af9197fa78b80cd47159bf7293bd03',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
